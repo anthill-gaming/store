d33700d79716609057b6ef71db6087e17275077d
Vladimir <x55aah@gmail.com>
2019-04-19 19:40:48 +0300

    update store models

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 # For more details, see
 # http://docs.sqlalchemy.org/en/latest/orm/tutorial.html#declare-a-mapping
 from anthill.framework.db import db
-from sqlalchemy_utils.types.json import JSONType
+from anthill.framework.utils import timezone
+from anthill.framework.utils.translation import translate_lazy as _
+from sqlalchemy_utils.types import JSONType, ChoiceType
 
 
 class Store(db.Model):
@@ -10,18 +12,47 @@ class Store(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(128), nullable=True, unique=True)
     payload = db.Column(JSONType, nullable=False, default={})
+    orders = db.relationship('Order', backref='store', lazy='dynamic')
+    items = db.relationship('Item', backref='store', lazy='dynamic')
+    active = db.Column(db.Boolean, nullable=False, default=True)
 
 
 class Item(db.Model):
     __tablename__ = 'items'
+    __table_args__ = (
+        db.UniqueConstraint('store_id', 'name'),
+    )
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    name = db.Column(db.String(128), nullable=True)
+    orders = db.relationship('Order', backref='item', lazy='dynamic')
+    active = db.Column(db.Boolean, nullable=False, default=True)
 
 
 class Order(db.Model):
     __tablename__ = 'orders'
 
+    STATUSES = (
+        ('new', _('New')),
+        ('created', _('Created')),
+        ('failed', _('Failed')),
+        ('retry', _('Retry')),
+        ('approved', _('Approved')),
+        ('rejected', _('Rejected')),
+        ('succeeded', _('Succeeded')),
+    )
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    status = db.Column(ChoiceType(STATUSES), default='new')
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+    user_id = db.Column(db.Integer, nullable=False)
+    currency_id = db.Column(db.Integer, db.ForeignKey('currencies.id'))
+    payload = db.Column(JSONType, nullable=False, default={})
+    count = db.Column(db.Integer, default=1)
+    created = db.Column(db.DateTime, default=timezone.now)
+    updated = db.Column(db.DateTime, onupdate=timezone.now)
 
 
 class Currency(db.Model):
